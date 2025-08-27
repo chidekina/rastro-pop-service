0919a1dffced0ba1fa7196aeb16a4ecf844e63d5
chidekina <cesar.nagano1@gmail.com>
2025-08-27 15:15:21 -0300

    config: initial config with express

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "127.0.0.1";
+
+app.listen(PORT, HOST, () => {
+    console.log(`Servidor escutando em http://${HOST}:${PORT}`);
+});
