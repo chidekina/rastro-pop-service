d46ed832a4cc3aa01a2501c12ddd714d442df205
chidekina <cesar.nagano1@gmail.com>
2025-08-27 16:52:34 -0300

    fix: add routes for server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("./src/moduleAlias");
 const express = require("express");
+const routes = require("@/routes");
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "127.0.0.1";
